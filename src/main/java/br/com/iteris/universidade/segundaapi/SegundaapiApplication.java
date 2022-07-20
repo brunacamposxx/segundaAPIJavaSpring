f49a04b8217bc6a14b8d39d7d3ffba7a4344094b
Bruna Campos Ferreira <bruna.ferreira@iteris.com.br>
2022-07-20 11:18:19 -0300

    Initial Config

---
+++ src/main/java/br/com/iteris/universidade/segundaapi/SegundaapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.iteris.universidade.segundaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iteris/universidade/segundaapi/SegundaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iteris.universidade.segundaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
